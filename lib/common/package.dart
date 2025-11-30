16228fdf53b430f68b5a41a421cd4604ca77d34b
tianquege <wangn5635@gmail.com>
2025-11-30 21:54:32 +0800

    1

--- lib/common/package.dart
+++ lib/common/package.dart
@@ -6,7 +6,7 @@ import 'common.dart';
 
 extension PackageInfoExtension on PackageInfo {
   String get ua => [
-        '$appName/v$version',
+        'TianQue/v$version',
         'clash-verge',
         'Platform/${Platform.operatingSystem}',
       ].join(' ');
